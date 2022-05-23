0e3d4840c22f2a6c0bf120d42d47c986a007fc73
Elmer Chanquin <elmer@em.gt>
2022-05-23 13:59:59 -0600

    Add settings page

---
+++ pages/app/settings.js
@@ -0,0 +1,13 @@
+import LayoutApp from "../../components/LayoutApp";
+
+const Settings = () => {
+  return (
+    <LayoutApp>
+      <div className="container mx-auto">
+        <h1 className="text-4xl px-4 md:px-0">Settings</h1>
+      </div>
+    </LayoutApp>
+  );
+};
+
+export default Settings;
