344062347395ddc216c791f0f9caa21e226ca964
Elmer Chanquin <elmer@em.gt>
2022-07-13 22:18:45 -0600

    Add CORS to index

--- pages/api/patients/index.js
+++ pages/api/patients/index.js
@@ -1,18 +1,38 @@
-import NextCors from "nextjs-cors";
+import Cors from "cors";
 import { connectToDatabase } from "@middleware/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler(req, res, ){
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
 
-    const {filters} = req.body;
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
 
-    const {db} = await connectToDatabase();
-    
-    
-    try {
-        const data = await db.collection("patients").find({}).limit(/* filters.limit */20).toArray();
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(500).json({error: 'Failed to load data'})
-    }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
+  const { filters } = req.body;
+
+  const { db } = await connectToDatabase();
+
+  try {
+    const data = await db
+      .collection("patients")
+      .find({})
+      .limit(/* filters.limit */ 20)
+      .toArray();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to load data" });
+  }
 }
