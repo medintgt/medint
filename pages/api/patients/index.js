db25458fb82e9bc26ed95b0b87d6626fc2d4a3f8
Elmer Chanquin <elmer@em.gt>
2022-07-13 22:01:38 -0600

    Update api with CORS package

--- pages/api/patients/index.js
+++ pages/api/patients/index.js
@@ -1,12 +1,18 @@
+import NextCors from "nextjs-cors";
 import { connectToDatabase } from "@middleware/database";
 import { ObjectId } from "mongodb";
 
-export default async function handler(req, res){
+export default async function handler(req, res, ){
+
     const {filters} = req.body;
 
     const {db} = await connectToDatabase();
     
-    const data = await db.collection("patients").find({}).limit(/* filters.limit */20).toArray();
     
-    res.json(data);
+    try {
+        const data = await db.collection("patients").find({}).limit(/* filters.limit */20).toArray();
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({error: 'Failed to load data'})
+    }
 }
