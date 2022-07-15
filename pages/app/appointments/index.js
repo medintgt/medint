e99586cd8bc0f005697c18e52010c07b3ffcb3da
Elmer Chanquin <elmer@em.gt>
2022-07-15 16:11:58 -0600

    Change admin pages to app

--- pages/app/appointments/index.js
+++ pages/app/appointments/index.js
@@ -1,12 +1,22 @@
-import LayoutApp from "../../../components/LayoutApp";
+import React, { useEffect, useState } from "react"
+import { ShowAppointments } from "@components/Appointments/ShowAppointments";
+import Title from "@components/Title";
+import QuickActions from "@components/Buttons/QuickActions";
+import ShowAllLink from "@components/Show/Elements/Link/ShowAllLink"
 
-export default function Appointments() {
-  return (
-    <LayoutApp>
-      <div className="container mx-auto">
-        <h1 className="text-4xl">Citas</h1>
-      </div>
-    </LayoutApp>
-  )
+const Appointments = () => {
+    return (
+        <div className="container mx-auto grid justify-center">
+            <Title 
+            title="Appointments"
+            src="/"
+            breadcrumb="Dashboard"
+            />
+            <ShowAppointments />
+            <QuickActions />
+            <ShowAllLink text="Show all appointments" link="/appointments/all" />
+        </div>
+    )
 }
 
+export default Appointments;
