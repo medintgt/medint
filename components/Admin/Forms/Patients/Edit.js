e65ef6f940639a5f7334efc62a1810c94b3b181a
Elmer Chanquin <elmer@em.gt>
2022-07-17 08:20:14 -0600

    Add api request from front

--- components/Admin/Forms/Patients/Edit.js
+++ components/Admin/Forms/Patients/Edit.js
@@ -1,47 +1,67 @@
 import React from "react";
-import Input from "@components/Forms/Elements/Input";
-import Textarea from "@components/Forms/Elements/Textarea";
-import PrimaryButton from "@components/Buttons/PrimaryButton";
-import Select from "@components/Forms/Elements/Select";
+import Input from "@components/Admin/Forms/Elements/Input";
+import Textarea from "@components/Admin/Forms/Elements/Textarea";
+import PrimaryButton from "@components/Admin/Buttons/PrimaryButton";
+import Select from "@components/Admin/Forms/Elements/Select";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const EditPatient = () => {
+const EditPatient = (props) => {
   const countries = [
-    { value: "0", text: "Guatemala", selected: "true" },
+    { value: "GTM", text: "Guatemala", selected: "true" },
     { value: "1", text: "El Salvador" },
   ];
   const options = [
-    { value: "0", text: "Male" },
-    { value: "1", text: "Female" },
+    { value: "0", text: "Masculino" },
+    { value: "1", text: "Femenino" },
   ];
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`/api/patients/patient/${props.id}`);
+      setData(response.data);
+    };
+    getData();
+  }, [props.id]);
+  const gender = ["Masculino", "Femenino"];
+  if (data != null) {
   return (
     <form className="my-4 max-w-md  md:w-96 h-auto p-3" action="">
       <Input
         label="User"
         placeholder="Select user (if applicable)"
         type="text"
+        value={data.user_id}
       />
-      <Input label="Name" placeholder="John" type="text" value="Elmer" />
+      <Input label="Name" placeholder="John" type="text" value={data.first_name} />
       <Input
         label="Midle Name"
         placeholder="Richard"
         type="text"
-        value="Alejandro"
+        value={data.middle_name}
       />
-      <Input label="Last Name" placeholder="Doe" type="text" value="Chanquin" />
+      <Input label="Last Name" placeholder="Doe" type="text" value={data.last_name} />
       <Input
         label="DPI/CUI"
         placeholder="1234567890987"
         type="number"
-        value="3029987800108"
+        value={data.dpi}
       />
-      <Input label="Birth date" type="date" value="1999-12-23" />
-      <Select label="Country" options={countries} value="1" />
-      <Select label="Gender" options={options} />
+      <Input label="Birth date" type="date" value={data.born_date} />
+      <Select label="Country" options={countries} value={data.address.country} />
+      <Select label="Gender" options={options} value={data.gender} />
       <div className="pt-4 grid place-items-center">
         <PrimaryButton text="Update Patient" />
       </div>
     </form>
-  );
+  );} else {
+    return (
+        <div className="container mx-auto grid justify-center">
+            <h1 className="text-center text-lg">Data is being loaded...</h1>
+        </div>
+        )
+}
 };
 
 export default EditPatient;
