fc5379866bcbcc5db5d03cfe88ec60e69a84b0d5
Elmer Chanquin <elmer@em.gt>
2022-07-17 08:24:31 -0600

    Update profile page with layout

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,11 +1,15 @@
-import Title from "@components/Title"
-import ImportantInfo from "@components/Cards/ImportantInfo"
+import Title from "@components/Admin/Title"
+import ImportantInfo from "@components/Admin/Cards/ImportantInfo"
+import LayoutAdmin from "@components/LayoutAdmin"
+
 const Profile = () => {
     return (
+        <LayoutAdmin>
         <div className="container mx-auto grid justify-center">
             <Title title="Profile" src="/dashboard" breadcrumb="Dashboard"/>
             <ImportantInfo value1="12312031" value2="Traumeel S" />
         </div>
+        </LayoutAdmin>
     );
 };
 
