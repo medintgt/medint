67d31eb05b19439837eba8a7a33460975c632653
Elmer Chanquin <elmer@em.gt>
2022-07-17 08:19:26 -0600

    Update Secondary Button with location fucntion

--- components/Admin/Buttons/SecondaryButton.js
+++ components/Admin/Buttons/SecondaryButton.js
@@ -1,6 +1,9 @@
 const SecondaryButton = (props) => {
+    function goTo(location) {
+        window.location.href=location
+    }
     return (
-        <button type={props.type} className="cursor-pointer w-72 text-2xl h-12 rounded-full text-sky-800 text-center p-2 border-2 border-sky-800">{props.text}</button>
+        <button type={props.type} onClick={() => goTo(props.location)} className="cursor-pointer w-72 text-2xl h-12 rounded-full text-sky-800 text-center p-2 border-2 border-sky-800">{props.text}</button>
     )
 }
 export default SecondaryButton
