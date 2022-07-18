e989fcfc7e546e7da8b5929e064a6e00cbb5e86c
Elmer Chanquin <elmer@em.gt>
2022-07-18 09:37:10 -0600

    Update App Layout with footer

--- components/LayoutAdmin.js
+++ components/LayoutAdmin.js
@@ -1,11 +1,19 @@
 import React from "react";
 import NavbarAdmin from "@components/Admin/Navbar/Navbar";
+import Link from "next/link"
 
 const Layout = ({ children }) => {
   return (
     <div>
       <NavbarAdmin />
       {children}
+      <footer className="container mx-auto flex justify-end">
+            <Link href='/changelog'>
+            <a>
+            <p className="text-sky-800">Version 2.0.0</p>
+            </a>
+            </Link>
+            </footer>
     </div>
   );
 };
