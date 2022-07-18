ce0ef532ae147ba4a7efb88d2645cc29902e2444
Elmer Chanquin <elmer@em.gt>
2022-07-18 10:02:53 -0600

    Add Users app Layout

--- pages/app/users/index.js
+++ pages/app/users/index.js
@@ -1,9 +1,13 @@
+import LayoutAdmin from "@components/LayoutAdmin";
+
 const Users = () => {
     return (
+        <LayoutAdmin>
         <div>
             
         </div>
-        )
+        </LayoutAdmin>
+        );
 };
 
 export default Users;
