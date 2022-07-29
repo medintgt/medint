3c789f021918cbac1058ef0312f8eeafb154f63d
Elmer Chanquin <elmer@em.gt>
2022-07-29 11:14:47 -0600

    Update therapies page with terapies component

--- pages/therapies/index.js
+++ pages/therapies/index.js
@@ -1,10 +1,11 @@
-import Layout from "../../components/Layout";
+import Layout from "@components/Layout";
+import { ShowAllTherapies } from "@components/Therapies/ShowAllTherapies";
 
 const Therapies = () => {
   return (
     <Layout>
       <div className="container mx-auto pt-12">
-     
+      <ShowAllTherapies />
      </div>
     </Layout>
   );
