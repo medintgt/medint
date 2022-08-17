2f78aa3dcbf5673c0a5bef4951b873594da2fbeb
Elmer Chanquin <elmer@em.gt>
2022-08-17 14:49:26 -0600

    Update next link to build

--- components/Admin/Patients/SearchPatient.js
+++ components/Admin/Patients/SearchPatient.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import Link from "next/Link"
+import Link from "next/link";
 
 const SearchPatient = () => {
   const [search, setSearch] = useState("");
