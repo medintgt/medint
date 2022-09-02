fed4078604b276a06b50d65a965be11e552798b8
Elmer Chanquin <elmer@em.gt>
2022-09-02 16:05:40 -0600

    Update button color for delete

--- components/Admin/Appointments/AppointmentCard.js
+++ components/Admin/Appointments/AppointmentCard.js
@@ -36,7 +36,9 @@ export const ApointmentCard = (props) => {
         </div>
         <div className="pt-4 grid place-items-center">
           <SecondaryButton type="submit" text="Editar" />
-          <button type="button" className="cursor-pointer w-72 text-2xl h-12 rounded-full text-red-700 text-center p-2 border-2 border-sky-800">Borrar</button>
+          </div>
+        <div className="pt-4 grid place-items-center">
+          <button type="button" className="cursor-pointer w-72 text-2xl h-12 rounded-full text-red-700 text-center p-2 border-2 border-red-700">Borrar</button>
         </div>
       </section>
     );
