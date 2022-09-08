a2d6aa1a3a7b4b338614ec8e90f28b6ea8142f49
Elmer Chanquin <elmer@em.gt>
2022-09-07 19:34:01 -0600

    Update histories new api route with pages to specific patient and search one

--- pages/app/histories/index.js
+++ pages/app/histories/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Title from "@components/Admin/Title";
-import ShowHistoriesCard from "@components/Admin/Histories/ShowHistoriesCard";
 import QuickActions from "@components/Admin/Buttons/QuickActions";
 import SearchTab from "@components/Admin/Search/SearchTab";
 import LayoutAdmin from "@components/LayoutAdmin";
@@ -16,8 +15,6 @@ const Histories = () => {
         title="Buscar Historia"
         placeholder="Buscar por id, nombre o dpi..."
         />
-      <ShowHistoriesCard />
-      {/* Add Statistics: Most common diagnostic; Therapies on the month, week and day.; Most common treatment. */}
       <QuickActions />
     </div>
         </LayoutAdmin>
