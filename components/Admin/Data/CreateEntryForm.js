76739aad479947d2396bbc0fcea36543d22f504b
Elmer Chanquin <elmer@em.gt>
2022-09-07 19:28:14 -0600

    Update create entry form to create data

--- components/Admin/Data/CreateEntryForm.js
+++ components/Admin/Data/CreateEntryForm.js
@@ -1,74 +1,271 @@
 import React from "react";
-import Input from "@components/Admin/Forms/Elements/Input";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import PrimaryButton from "@components/Admin/Buttons/PrimaryButton";
+import SearchPatientInput from "@components/Admin/Data/SearchPatientInput";
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 const CreateEntryForm = () => {
+  const [search, setSearch] = useState("");
+  let [date, onChange] = useState(new Date());
   const options = [
-    { value: "0", text: "Blood Pressure" },
-    { value: "1", text: "Height" },
-    { value: "2", text: "Weight" },
-    { value: "3", text: "Heart Rate" },
-    { value: "4", text: "Respiratory Rate" },
-    { value: "5", text: "Temperature" },
-    { value: "6", text: "Pulse" },
-    { value: "7", text: "Oxigen Saturation" },
+    { id: "0", name: "Presión en sangre" },
+    { id: "1", name: "Altura" },
+    { id: "2", name: "Peso" },
+    { id: "3", name: "Frecuencia cardíaca" },
+    { id: "4", name: "Frecuencia respiratoria" },
+    { id: "5", name: "Temperatura" },
+    { id: "6", name: "Pulso" },
+    { id: "7", name: "Saturación de oxígeno" },
   ];
+  const [patient, setPatient] = useState({
+    name: "",
+    id: ""
+  });
+  const [state, setState] = useState("0");
+  const [data, setData] = useState({
+    date: date.toISOString().slice(0, 10),
+    patient: patient,
+    type: 0,
+    value1: null,
+    value2: null
+  });
 
-  const [state, setState] = React.useState("0");
+  const sendData = async () => {
+    const response = await axios.post(`/api/data/new/`, data);
+    let responseData = response.data;
+    if (responseData.acknowledged == true) {
+      window.location.href = `/app/patients/${data.patient.id}`;
+    } else {
+      window.alert("Hubo un error.")
+    }
+  };
 
   const select = options.map((option) => (
-    <option key={option.value} value={option.value}>
-      {option.text}
+    <option key={option.id} value={option.id}>
+      {option.name}
     </option>
   ));
   const selectWithSetValue = (
     <select
-      id="data-options"
+      id="type_list"
       className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
       name=""
-      onChange={() => setState(document.querySelector("#data-options").value)}
+      onChange={() => {setState(document.querySelector("#type_list").value)}}
+      
     >
       {select}
     </select>
   );
+  useEffect(() => {
+    const handleDateChange = () => {
+      let updatedData = {
+        date: date.toISOString().slice(0, 10),
+      };
+      setData((data) => ({
+        ...data,
+        ...updatedData,
+      }));
+    };
+    handleDateChange();
+  }, [date]);
+  useEffect(() => {
+    const handleTypeChange = () => {
+      let updatedData = {
+        type: state,
+      };
+      setData((data) => ({
+        ...data,
+        ...updatedData,
+      }));
+    };
+    handleTypeChange();
+  }, [state]);
+  useEffect(() => {
+    const handlePatientChange = () => {
+      let updatedData = {
+        patient: patient,
+      };
+      setData((data) => ({
+        ...data,
+        ...updatedData,
+      }));
+    };
+    handlePatientChange();
+  }, [patient]);
+  const handleChangeWithValue = (value, type) => {
+    let updatedData = {
+      [type]: value,
+    };
+    setData((data) => ({
+      ...data,
+      ...updatedData,
+    }));
+  };
   return (
     <form>
-      <Input placeholder="Jhon Doe" label="Patient" />
+      <SearchPatientInput
+        search={search}
+        setSearch={setSearch}
+        setPatient={setPatient}
+      />
       <div>
-        <label className="text-lg text-gray-400">Data</label>
+        <label className="text-lg text-gray-400">Dato</label>
       </div>
       <div className=" pt-2 grid place-items-center">{selectWithSetValue}</div>
-      <DataType state={state}></DataType>
-      <Calendar className="my-4" />
+      <DataType state={state} data={data} handleChangeWithValue={handleChangeWithValue}></DataType>
+      <Calendar
+          className="mt-4 rounded-md"
+          onChange={onChange}
+          value={date}
+          minDate={new Date()}
+        />
       <div className=" pt-2 grid place-items-center">
-        <PrimaryButton text="Save" type="button" />
+      <button type="button" className="cursor-pointer w-72 text-2xl bg-sky-800 h-12 rounded-full text-white text-center p-2" onClick={sendData}>Agregar dato</button>
       </div>
     </form>
   );
 };
 
-const DataType = ({ state }) => {
+const DataType = ({state, data, handleChangeWithValue}) => {
   if (state == "0") {
-    return [
-      <Input key='0' placeholder="80" label="Diastolic" type="number" />,
-      <Input key='1' placeholder="120" label="Systolic" type="number" />,
-    ];
+    return (
+      <div>
+        <div>
+      <label className="text-lg text-gray-400">Diastólica</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="80"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>
+    <div>
+      <label className="text-lg text-gray-400">Sistólica</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="80"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value2"
+          type="number"
+          value={data.value2}
+        ></input>
+      </div>
+    </div>
+      </div>
+    );
   } else if (state == "1") {
-    return <Input placeholder="170" label="Height" type="number" />;
+    return <div>
+    <label className="text-lg text-gray-400">Altura</label>
+    <div className="pt-2 grid place-items-center">
+      <input
+        key='0'
+        onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+        placeholder="80"
+        className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+        name="value1"
+        type="number"
+        value={data.value1}
+      ></input>
+    </div>
+  </div>;
   } else if (state == "2") {
-    return <Input placeholder="120" label="Weight" type="number" />;
+    return (<div>
+      <label className="text-lg text-gray-400">Peso (Libras)</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="150"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   } else if (state == "3") {
-    return <Input placeholder="80" label="Heart Rate" type="number" />;
+    (<div>
+      <label className="text-lg text-gray-400">Frecuencia cardíaca</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="150"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   } else if (state == "4") {
-    return <Input placeholder="18" label="Respiratory Rate" type="number" />;
+    (<div>
+      <label className="text-lg text-gray-400">Frecuencia respiratoria</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="150"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   } else if (state == "5") {
-    return <Input placeholder="37" label="Temperature" type="number" />;
+    (<div>
+      <label className="text-lg text-gray-400">Temperatura</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="150"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   } else if (state == "6") {
-    return <Input placeholder="80" label="Pulse" type="number" />;
+    (<div>
+      <label className="text-lg text-gray-400">Pulso</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="80"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   } else if (state == "7") {
-    return <Input placeholder="98" label="Oxigen Saturation" type="number" />;
+    (<div>
+      <label className="text-lg text-gray-400">Saturación de oxígeno</label>
+      <div className="pt-2 grid place-items-center">
+        <input
+          key='0'
+          onChange={(e) => {handleChangeWithValue(e.target.value, e.target.name)}}
+          placeholder="150"
+          className="mx-auto w-72 py-1 px-2 border rounded-md border-gray-400"
+          name="value1"
+          type="number"
+          value={data.value1}
+        ></input>
+      </div>
+    </div>)
   }
 };
 export default CreateEntryForm;
