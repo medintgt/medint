fa52210666e4e8f0719838a8786a6d9b2de22e23
Elmer Chanquin <elmer@em.gt>
2022-09-20 11:07:03 -0600

    Update finances index page to show link that goes to ledger page

--- pages/app/finances/index.js
+++ pages/app/finances/index.js
@@ -3,6 +3,7 @@ import Title from "@components/Admin/Title"
 import ImportantInfo from "@components/Admin/Cards/ImportantInfo";
 import QuickActions from "@components/Admin/Buttons/QuickActions";
 import LayoutAdmin from "@components/LayoutAdmin";
+import Link from "next/link";
 
 
 const Movements = () => {
@@ -15,6 +16,9 @@ const Movements = () => {
             src="/"
             breadcrumb="Escritorio"
             />
+            <Link href={"/app/finances/ledger"}>
+            <a className="cursor-pointer text-sky-800">Ir a registros</a>
+            </Link>
             <ImportantInfo value1="Important info" value2="Important info" />
             <QuickActions />
         </div>
