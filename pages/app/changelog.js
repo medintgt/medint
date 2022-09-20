7a1d32a87087f79040db1fee899647b4d4289eb5
Elmer Chanquin <elmer@em.gt>
2022-09-20 11:17:28 -0600

    Update changelog page with last update

--- pages/app/changelog.js
+++ pages/app/changelog.js
@@ -7,6 +7,9 @@ const ChangeLog = () => {
 
     <div className="container mx-auto py-8">
       <h3 className="text-3xl">Changelog</h3>
+      <strong>V.2.0.2</strong>
+      <p>- Esconder resultados al seleccionar paciente en historia clinica</p>
+      <br/>
       <strong>V.2.0.1</strong>
       <p>- Encontrar pacientes esconde resultados al seleccionar.</p>
       <br/>
